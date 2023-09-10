f2c6f572e0983b2ef3d4def599681e6ebdc2161b
jjnch <jjnunezchao1@gmail.com>
2023-09-09 22:50:18 -0600

    actualizacion 3

--- src/componentes/Contador/index.jsx
+++ src/componentes/Contador/index.jsx
@@ -2,23 +2,29 @@ import React, { useState } from "react";
 
 export const Contador = () => {
     const [ count, setCount ] = useState(1);
+    const [ habilitado, setHabilitado ] = useState(false);
 
     const sumar = () => {        
         setCount(count + 1);
+        if (count > -1)
+            setHabilitado(false);
     }
 
     const restar = () => {        
         setCount(count - 1);
-
-        if (count <= 0)
-            setCount(0);            
+        if (count <= 1)
+        {
+            setCount(0);
+            setHabilitado(true);
+        }
+            
     }
 
     
     return (
         <>
             <div>
-                <button className="btn btn-primary" onClick={restar}>-</button>
+                <button className="btn btn-primary" onClick={restar} disabled = { habilitado }>-</button>
                 <span> {count} </span>
                 <button className="btn btn-primary" onClick={sumar}>+</button>
             </div>
