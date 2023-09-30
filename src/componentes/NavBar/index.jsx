1103aaa0728a4d26bce2829f629517c8deeaa00c
jjnch <jjnunezchao1@gmail.com>
2023-09-30 14:47:31 -0600

    Actualización 30 sep

--- src/componentes/NavBar/index.jsx
+++ src/componentes/NavBar/index.jsx
@@ -4,7 +4,7 @@ import "./NavBar.css";
 export const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Mi Sitio</a>
+            <a className="navbar-brand" href="#">My Chao</a>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
