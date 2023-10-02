ca8cbebbf73fb5c4b043217e3bec50d74692ae06
jjnch <jjnunezchao1@gmail.com>
2023-10-02 06:59:11 -0600

    update 02102023

--- src/componentes/NavBar/index.jsx
+++ src/componentes/NavBar/index.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NavBar.css";
 
 export const NavBar = () => {
+
+    const [categorias, setCategorias] = useState([]);
+
+    useEffect(() => {
+        fetch ('https://chaovirtual.com/P1/api/categorias')
+        .then((response) => response.json())
+        .then((data) => { 
+            console.log(data);
+            setCategorias(data);
+            })
+        .catch((error) => console.error('Error al obtener categorias:', error));
+
+    }, []);
+
     return (
-        <div className="header">
+        <div className="header" style={{ boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.1)' }}>
             <nav className="navbar navbar-expand-lg navbar-light bg-light" name="nav1">
                 <div className="container-fluid">
                     <div className="col-1">
@@ -13,31 +27,28 @@ export const NavBar = () => {
                         <input type="text" className="form-control" placeholder="Buscar palabras" />
                     </div>
                     <div className="col-1 text-center">
-                        <a href="#">Iniciar Sesión <i className="fas fa-sign-in-alt"></i></a>
+                        <a href="#" style={{ textDecoration: 'none', color: 'black'}} >
+                            <i className="fa fa-user"></i>
+                            <div style={{ fontSize: '10px', margin: -3}}>Iniciar Sesión</div>
+                        </a>
                     </div>
                 </div>
                 
             </nav>
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light" name="nav2">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light mx-auto" name="nav2">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
                     <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Inicio</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Acerca de</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Servicios</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Contacto</a>
-                    </li>
+                        {categorias.map((categoria, index) => (
+                            <li className="nav-item active" key={index}>
+                            <a className="nav-link" href="#">{categoria.categoria}</a>
+                        </li>        
+                        ))}
+
                     </ul>
                 </div>
             </nav>
